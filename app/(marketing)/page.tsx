73ed4f0e10d36cb7f72cac3f9a9c9b6740f5a622
Suchitra Swain <suchitraswain.2012@gmail.com>
2024-12-12 14:00:25 +0100

    board page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,20 +1,31 @@
+import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Medal } from "lucide-react";
 import { Poppins } from "next/font/google";
+import Link from "next/link";
 import React from "react";
+import localFont from "next/font/local";
 
 const textFont = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
+const headingFont = localFont({
+  src: "../../public/fonts/font.woff2",
+});
 
 const MarketingPage = () => {
   return (
-    <div className="flex item-center justify-center flex-col">
-      <div className="flex items-center justify-center flex-col">
+    <div className="flex items-center justify-center flex-col">
+      <div
+        className={cn(
+          "flex items-center justify-center flex-col",
+          headingFont.className
+        )}
+      >
         <div
-          className="mb-4 flex items-center border shadow-sm p-4
-        bg-amber-100 text-amber-700 rounded-full uppercase"
+          className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase"
+          data-testid="main_award"
         >
           <Medal className="h-6 w-6 mr-2" />
           No 1 task managment
@@ -43,6 +54,9 @@ const MarketingPage = () => {
         high rises to the home office, the way your team works is unique -
         accomplish it all with Taskify.
       </div>
+      <Button className="mt-6" size="lg" asChild data-testid="main_button">
+        <Link href="/sign-up">Get Taskify for free</Link>
+      </Button>
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { siteConfig } from "@/config/site";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    default: "Taskify - Trello Clone",
-    template: `%s | Canban Clone`,
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
   },
-  description: "Fullstack Trello Clone",
+  description: siteConfig.description,
   icons: [
     {
       url: "/logo.svg",
